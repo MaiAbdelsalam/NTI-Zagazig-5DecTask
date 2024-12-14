b0f8a52fe9331d8fdd98b3f2b144a6144ac92aaa
MaiAbdelsalam <118804478+MaiAbdelsalam@users.noreply.github.com>
2024-12-14 15:10:41 +0200

    Delete backend directory

--- backend/src/category/categories.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from "mongoose";
-export interface Categories extends Document{
-   readonly name:string;
-    image:string;
-};
-
--- backend/src/subcategries/subcategories.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Categories } from "../category/categories.interface";
-import { Document } from "mongoose";
-export interface subcategoriesInterface extends Document{
-    readonly name:string;
-    readonly category:Categories;
-    image:string;
-}
