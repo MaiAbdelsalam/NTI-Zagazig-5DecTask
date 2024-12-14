c970316dcd92369217ec5ab7f239cbc46b720865
MaiAbdelsalam <118804478+MaiAbdelsalam@users.noreply.github.com>
2024-12-14 15:08:25 +0200

    Delete dist directory

--- dist/srctraning/config/dtabase.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import mongoose from "mongoose";
-// const dbConnection=mongoose.connect("mongodb://localhost:27017/Nti-e-commerce")
-// .then(()=>{
-//     console.log("db connected");
-// }).catch((err)=>{console.log(err)});
-// export default dbConnection;
