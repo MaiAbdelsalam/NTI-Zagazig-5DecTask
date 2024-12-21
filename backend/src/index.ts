138812a20ba8fa7c6f955bafc7eafa41316d59d1
MaiAbdelsalam <maiabdelsalam206@gmail.com>
2024-12-21 18:42:05 +0200

    googeleauthentication/wishlist/address

---
+++ backend/src/index.ts
@@ -0,0 +1,72 @@
+import categoriesRouter from "./category/categories.routes";
+import subcategoriesRouter from "./subcategries/subcategories.route";
+import globalErrors from "./middelwaresErrors/middelwares.error";
+import ApiErrors from "./utilsuses/ApiErrors";
+import express from 'express';
+import { Request,Response,NextFunction } from "express";
+import productRouter from "./product/product.route";
+import userRouter from "./user/user.routes";
+import authRouter from "./authenticator/auth.route"
+import { UserInterface } from "./user/user.interface";
+import profileRouter from "./profile/profile.routes";
+import googleRouter from "./google/google.router"
+import WishlistRouter from "./wishlist/wishlist.routes";
+import AddressRouter from "./address/address.routes";
+import ReviewsRouter from "./reviews/reviews.routes";
+// import googleRouter from
+// declare module "express"{
+//     interface Request{
+//         filterData?:any;
+//     }
+// }
+
+declare module  "express"{
+    interface Request{
+        filterData?:any
+        req?:any
+        files?:any
+        user?:any
+    }
+}
+
+const mountRoutes=(app:express.Application)=>{
+    app.use("/auth/google",googleRouter);
+    app.use("/api/v1/categories",categoriesRouter)
+    app.use('/api/v1/subcategories',subcategoriesRouter);
+    app.use('/api/v1/product',productRouter);
+    app.use("/api/v1/auth", authRouter);
+    app.use("/api/v1/user",userRouter)
+    app.use("/api/v1/profile",profileRouter)
+    app.use("/api/v1/wishlist",WishlistRouter)
+    app.use("/api/v1/address",AddressRouter)
+    app.use("/api/v1/reviews",ReviewsRouter)
+
+
+
+
+
+
+
+
+
+//errors    
+// error for notfound category or product  
+     app.all("*",(req:Request,res:Response,next:NextFunction)=>{
+        // const error= new Error('route/ele not found');
+        next(new ApiErrors(`route ${req.originalUrl} is not found please solve it`,400));
+        
+    });
+    app.use(globalErrors);
+
+    //  الفنكشن دي هي والفنكشن الموجوده اخر حاجه في ملف الاندكس كنت بستخدمهم عشان اعمل كرييت لل ايرور وابتدي 
+    // استخدمه لكن وقفتهم عشان هستبدلهم بملفات الميدويير الديفلوب والبرودكشن والجلوبال
+    // app.all("*",(req:express.Request,res:express.Response,next:NextFunction)=>{
+    //     const error= new Error('route/ele not found');
+    //     next(error);
+    // });
+
+
+};
+
+export default mountRoutes;
+
---
+++ backend/src/refactorysechmaservices.ts
@@ -0,0 +1,61 @@
+
+
+import express from 'express'
+import { Request,Response,NextFunction } from "express";
+import ApiErrors from './utilsuses/ApiErrors';
+import asyncHandler from "express-async-handler";
+import mongoose, { modelNames, mongo, Schema } from "mongoose";
+import { Document } from "mongoose";
+import Features from './utilsuses/features';
+class RefactorSchemaService{
+   
+   getAll=<modelType>(model:mongoose.Model<any>,modelName?:string)=>asyncHandler(async(req:express.Request,res:express.Response,next:express.NextFunction)=>{
+      let filterData:any={};
+      if(req.filterData) filterData=req.filterData;
+
+      let features = new Features(model.find(filterData), req.query).filter().sort().limitFields().search(modelName!);
+      let {moongoseQuery}=features;
+      
+      const document:modelType[]= await moongoseQuery;
+      const documentsCount =document.length
+      features=features.pagination(documentsCount)
+      let {paginationResult}=features;
+            console.log(documentsCount)
+      
+      res.status(200).json({pagination:paginationResult, length: documentsCount,data:document})
+      console.log(`pagination ${documentsCount}`)
+
+          console.log(req.query)
+ 
+   })
+
+   createOne=<modelType>(model:mongoose.Model<any>)=>asyncHandler(async(req:express.Request,res:express.Response,next:express.NextFunction)=>{
+      const Document: modelType= await model.create(req.body);
+      res.status(201).json({data:Document})
+   });
+
+   getOne=<modelType>(model:mongoose.Model<any>,modelName?:string,populationObj?:string)=>asyncHandler(async(req:express.Request,res:express.Response,next:express.NextFunction):Promise<void>=>{
+      let query: any= await model.findById(req.params.id);
+      if(populationObj) query=query.populate(populationObj)
+      const Document: any= await query;
+      if(!Document) return next(new ApiErrors(`${req.__('not_found')}`,404));
+
+      res.status(200).json({data:Document})
+   });
+
+   updateOne=<modelType>(model:mongoose.Model<any>)=>asyncHandler(async(req:express.Request,res:express.Response,next:express.NextFunction):Promise<void>=>{
+      const Document: modelType| null= await model.findOneAndUpdate({_id:req.params.id},req.body,{new:true});
+      if(!Document) return next(new ApiErrors(`${req.__('not_found')} `,404));
+      res.status(201).json({data:Document})
+   });
+   deleteOne=<modelType>(model:mongoose.Model<any>)=>asyncHandler(async(req:express.Request,res:express.Response,next:express.NextFunction)=>{
+
+      const Document:modelType|null=await model.findByIdAndDelete(req.params.id);
+      if(!Document) return next(new ApiErrors(`${req.__('not_found')} `,404));
+      res.status(204).json()
+   });
+}
+
+ const refactorSchemaService =new RefactorSchemaService() ;
+export default refactorSchemaService;
+
